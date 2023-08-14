42b98086e909effe9389f5644d6017c02bcb4051
Yuri Soares <yurisoaresm@outlook.com>
2023-08-14 16:55:14 -0300

    feat: implements refresh token

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import fastifyCookie from '@fastify/cookie';
 import fastifyJwt from '@fastify/jwt';
 import fastify from 'fastify';
 import { ZodError } from 'zod';
@@ -11,7 +12,11 @@ export const app = fastify();
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: { cookieName: 'refreshToken', signed: false },
+  sign: { expiresIn: '10m' },
 });
+
+app.register(fastifyCookie);
 
 app.register(usersRoutes);
 app.register(gymsRoutes);
--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify';
 
 import { authenticate } from './authenticate.controller';
 import { profile } from './profile.controller';
+import { refresh } from './refresh.controller';
 import { register } from './register.controller';
 
 import { verifyJWT } from '@/http/middlewares/verify-jwt';
@@ -11,4 +12,6 @@ export async function usersRoutes(app: FastifyInstance) {
 
   app.post('/users', register);
   app.post('/sessions', authenticate);
+
+  app.patch('/token/refresh', refresh);
 }
